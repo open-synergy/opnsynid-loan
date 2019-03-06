9196b098d0973d3b9009b2ccb9d249ea9685922f
Andhitia Rama <andhitia.r@gmail.com>
2019-03-07 00:01:21 +0700

    [8.0.1.0.0] loan_core

--- loan_core/wizard/realize_interest.py
+++ loan_core/wizard/realize_interest.py
@@ -9,20 +9,20 @@ from openerp.tools.translate import _
 
 
 class RealizeInterst(models.TransientModel):
-    _name = "hr.loan.realize_interest"
+    _name = "loan.realize_interest"
     _description = "Realize Loan Interest"
 
     date_realization = fields.Date(
         string="Date Realization",
-        required=True,
+        required=False,
         default=datetime.now().strftime("%Y-%m-%d"),
     )
 
     @api.multi
     def action_realize(self):
         self.ensure_one()
-        obj_schedule = self.env[
-            "hr.loan.payment.schedule"]
+        model_name = self._context.get("active_model", False)
+        obj_schedule = self.env[model_name]
         schedule_ids = self.env.context.get("active_ids", False)
         if not schedule_ids or len(schedule_ids) == 0:
             strWarning = _("No loan repayment schedule selected")
